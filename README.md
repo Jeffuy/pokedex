e451f0f476acc20ddbafad9bca92220e49c25689
Jeff Free <jefffreelance3@gmail.com>
2022-04-28 15:12:56 -0300

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to the Pokedex!
+
+The pokedex was created using vanilla JS and [the Pokemon Api](https://pokeapi.co/) for API´s Platzi Course.
+
+
+# Snapshot
+![Snapshot](https://i.ibb.co/8Y9wfRf/image.png)
---
+++ src/script/index.js
@@ -0,0 +1,58 @@
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+document.querySelector("#pokeButton").addEventListener("click", pokeButton);
+document.addEventListener("DOMContentLoaded", pokeButton);
+
+let container = document.querySelector("#container");
+
+function pokeButton() {
+    let random = Math.floor(Math.random() * 251) + 1;
+    console.log(random);
+    catchEmAll(random);
+}
+
+function pokeShow(pokeName, pokeImg, pokeType) {
+    //limpio el container
+    container.innerHTML = "";
+
+    //creo el card
+    let card = document.createElement("div");
+	card.className = "card";
+
+    //creo el titulo
+    let name = document.createElement("h2");
+    name.innerText = pokeName.toUpperCase();
+    card.appendChild(name);
+	name.className = "card-title";
+
+    //creo la imagen
+    let img = document.createElement("img");
+    img.src = pokeImg;
+    card.appendChild(img);
+	img.className = "card-img";
+
+    //creo el tipo
+	let typeTag = document.createElement("p");
+	typeTag.innerText = "TYPE:";
+	typeTag.className = "card-typeTag";
+    let type = document.createElement("p");
+    type.innerText = pokeType.toUpperCase();
+	card.appendChild(typeTag);
+    card.appendChild(type);
+	type.className = "card-type";
+
+    //pego todo en el container
+    container.appendChild(card);
+}
+
+function catchEmAll(random) {
+    fetch(URL + random)
+        .then((res) => res.json())
+        .then((data) => {
+            let pokeImage = data.sprites.other.dream_world.front_default;
+            let pokeName = data.name;
+            let pokeType = data.types[0].type.name;
+
+            pokeShow(pokeName, pokeImage, pokeType);
+        });
+}
